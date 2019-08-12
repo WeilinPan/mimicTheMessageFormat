9ef2b43ec2055bde4bef0917e0444bd2c3505a7e
apan <s71440@gmail.com>
2019-08-12 10:01:21 +0800

    Initial Commit

---
+++ mimicTheMessageFormat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mimicTheMessageFormat
+//
+//  Created by APAN on 2019/8/12.
+//  Copyright © 2019 APAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
